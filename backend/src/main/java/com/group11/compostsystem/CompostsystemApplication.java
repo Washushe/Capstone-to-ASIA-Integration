9b070c0d780f2a908daabb426450a04c32f85d67
Mike Jhon G. Marquez <marquezmikejhon21@gmail.com>
2026-05-08 04:36:00 +0800

    Add Spring Boot backend

---
+++ backend/src/main/java/com/group11/compostsystem/CompostsystemApplication.java
@@ -0,0 +1,13 @@
+package com.group11.compostsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompostsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompostsystemApplication.class, args);
+	}
+
+}
